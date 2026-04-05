e3f9f8cc72a604e17cf6ea377963306289c1c1b2
glauco.r3 <glauco.r3@gmail.com>
2026-04-04 22:46:15 -0300

    Ajuste Worklist

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -10,6 +10,11 @@ sap.ui.define([
         return Controller.extend("cadmatv2.controller.Worklist", {
             onInit: function () {
 
+            },
+            onFilterBarInitialise: function (oEvent) {
+                let oSmartFilter = oEvent.getSource();
+                // Agora é seguro usar oSmartFilter.getFilters()
+                console.log("SmartFilterBar pronto para uso!");
             }
         });
     });
